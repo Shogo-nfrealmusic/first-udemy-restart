67916157aa8962acff33ce1096a491d51265db7c
Shogo <shogo.baseball0306@gmail.com>
2024-02-27 16:33:25 +0900

    how to define function

---
+++ material-for-windows (1)/front-dev-tutorial/04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,14 @@
+const hello = function (name, age) {
+    // let name = 'code mafia';
+    console.log('hello' + name + age);
+
+    return name + age;
+}
+
+hello();
+
+
+
+hello('Code Mafia', 10);
+const returnVal = hello('Code Mafia2', 20);
+console.log(returnVal);

cf30bed35fcf5de8a87b3d51d9542ba71b9c271d
Shogo <shogo.baseball0306@gmail.com>
2024-02-27 16:18:52 +0900

    variable of js and form of js

---
+++ material-for-windows (1)/front-dev-tutorial/04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,12 @@
+
+let name = 'Tom';
+name = 'Tim';
+
+console.log ('hello' + name);
+
+let variable = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+
+console.log(typeof variable);

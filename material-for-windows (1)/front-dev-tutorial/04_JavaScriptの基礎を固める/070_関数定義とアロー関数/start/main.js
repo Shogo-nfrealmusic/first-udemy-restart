b889bd33f8b02a82b0593c1541fc4de188336c6d
Shogo <shogo.baseball0306@gmail.com>
2024-02-29 11:42:35 +0900

    arrow function

---
+++ material-for-windows (1)/front-dev-tutorial/04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,10 @@
+// function hello (name = 'Tom') {
+//     console.log('hello' + name);
+// }
+
+const hello = (name , age) => 40;
+
+const arry = [1,2,3,4,5];
+
+arry.forEach(value => console.log (value));
+

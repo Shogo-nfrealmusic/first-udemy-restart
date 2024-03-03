aab421f11e41d91da06400e8afd96a5146286f4d
Shogo <shogo.baseball0306@gmail.com>
2024-03-03 18:16:15 +0900

    mobile-menu part.1

--- material-for-windows (1)/front-dev-tutorial/05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
+++ material-for-windows (1)/front-dev-tutorial/05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -1,19 +1,25 @@
 class MobileMenu{
-    constructor(){
-    this.DOM={};
-    this.DOM.btn=document.querySelector('.mobile-menu__btn');
-    this.DOM.container=document.querySelector('#global-container');
-    this.eventType=this._getEventType();
-    //this._addEvent();
-  this.DOM.btn.addEventListener('click',this._toggle.bind(this));
+    constructor() {
+    this.DOM = {};
+    this.DOM.btn = document.querySelector('.mobile-menu__btn');
+    this.DOM.container = document.querySelector('#global-container');
+    this.eventType = this._getEventType();
+    this._addEvent();
+}
+
+_getEventType() {
+  const isTouchCapable = "ontouchstart" in window;
+
+    return isTouchCapable ? "touchstart" : "click";
 }
  
- 
-_toggle(){
+_toggle() {
     this.DOM.container.classList.toggle('menu-open');
     }
- // _addEvent(){
-     //   this.DOM.btn.addEventListener('click',this._toggle.bind(this))
-   //}
+
+ _addEvent() {
+  this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+  this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+   }
 }
 new MobileMenu();

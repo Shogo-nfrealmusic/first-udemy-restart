b3d4d96a74edf4d9450dd2454e7fee2e2a9b1230
Shogo <shogo.baseball0306@gmail.com>
2024-03-08 20:09:00 +0900

    refactoring

--- material-for-windows (1)/front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
+++ material-for-windows (1)/front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
@@ -1,17 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hero = new HeroSlider('.swiper');
-    hero.start();
+    new Main;
+});
 
-    const _textAnimation = function (el, inview) {
-        if(inview) {
-            const ta = new TweenTextAnimation(el);
-            ta.animate();
-        }
+class Main {
+    constructor() {
+        this.header = document.querySelector('.header');
+        this.hero = new HeroSlider('.swiper');
+        this._observers = [];
+        this._init();
+        
     }
 
-    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
+    _init() {
+        new MobileMenu();
+        this._scrollInit();
+    }
 
-    const _inviewAnimation = function (el, inview) {
+    _scrollInit() {
+        this._observers.push(
+            new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false}),
+            new ScrollObserver('.swiper', this._toggleSlideAnimation.bind(this), {once: false}),
+            new ScrollObserver('.cover-slide', this._inviewAnimation),
+            new ScrollObserver('.tween-animate-title', this._textAnimation)
+        )
+        console.log(this._observers);
+    }
+
+    _toggleSlideAnimation(el, inview) {
+            if(inview) {
+                this.hero.start();
+            } else {
+                this.hero.stop();
+            }
+        }
+
+    _navAnimation(el, inview) {
+        if(inview) {
+            this.header.classList.remove('triggered');
+        } else {
+            this.header.classList.add('triggered');
+        }
+    }
+    
+    _inviewAnimation(el, inview) {
         if(inview) {
             el.classList.add('inview');
         } else {
@@ -19,19 +50,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
-
-    const header = document.querySelector('.header');
-    const _navAnimation = function (el, inview) {
+    _textAnimation(el, inview) {
         if(inview) {
-            header.classList.remove('triggered');
-        } else {
-            header.classList.add('triggered');
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
         }
     }
 
-    const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false});
 
-    new MobileMenu();
-});
+}
 

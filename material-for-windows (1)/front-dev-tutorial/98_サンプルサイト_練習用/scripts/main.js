7dfb9eecc034e0ddfd7e833164f0dc52ed7968ed
Shogo <shogo.baseball0306@gmail.com>
2024-03-08 20:21:09 +0900

    side animation and pace on done

--- material-for-windows (1)/front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
+++ material-for-windows (1)/front-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
@@ -6,6 +6,7 @@ class Main {
     constructor() {
         this.header = document.querySelector('.header');
         this.hero = new HeroSlider('.swiper');
+        this.sides = document.querySelectorAll('.side');
         this._observers = [];
         this._init();
         
@@ -13,11 +14,13 @@ class Main {
 
     _init() {
         new MobileMenu();
-        this._scrollInit();
+        Pace.on('done', this._scrollInit.bind(this));
     }
 
     _scrollInit() {
         this._observers.push(
+            new ScrollObserver('#main-content', this._sideAnimation.bind(this), {once: false,
+            rootMargin: "-300px 0px" }),
             new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false}),
             new ScrollObserver('.swiper', this._toggleSlideAnimation.bind(this), {once: false}),
             new ScrollObserver('.cover-slide', this._inviewAnimation),
@@ -39,6 +42,14 @@ class Main {
             this.header.classList.remove('triggered');
         } else {
             this.header.classList.add('triggered');
+        }
+    }
+
+    _sideAnimation(el, inview) {
+        if(inview) {
+            this.sides.forEach(side => side.classList.add('inview') );
+        } else {
+            this.sides.forEach(side => side.classList.remove('inview') );
         }
     }
     
